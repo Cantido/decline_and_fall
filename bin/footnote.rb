ad7d0b60f8d23a61b3403c10eb8d4dc7b0095f3e
Rosa Richter <rosa.richter@asynchrony.com>
2018-08-08 13:19:44 -0500

    Format all footnotes correctly

---
+++ bin/footnote.rb
@@ -0,0 +1,24 @@
+#!/usr/bin/env ruby
+
+ARGV.each do |file|
+  chapter = IO.read(file)
+
+  fnrx = /\[Footnote (\d+): ([^\]]*)\]/m
+  brackets = {
+    "[" => "(",
+    "]" => ")"
+  }
+
+  notes = {}
+
+  footnotes = chapter.scan(fnrx) { |n, s|
+    note = s.gsub(/(\[|\])/m, brackets)
+    notes["[#{n}]"] = "footnote:[#{note}]\n"
+  }
+
+  updated = chapter
+   .gsub(fnrx, "")
+   .gsub(/\[\d+\]/, notes)
+
+  IO.write(file, updated)
+end
